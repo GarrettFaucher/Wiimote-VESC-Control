b9de9e4399489baf0a2c22fc274957d4f56d0a76
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-04-10 13:25:37 -0400

    initial commit

---
+++ ESk8.py
@@ -0,0 +1,16 @@
+# Code for Rasberry Pi to interface with a VESC using a Wiimote
+
+import time
+import sys
+import threading
+import subprocess
+import cwiid
+import pigpio
+import pyvesc
+
+# Global Variables
+WIIMOTE_MAC = "00:1C:BE:25:8B:36"
+POWER_DOWN = ["sudo", "shutdown", "-h", "now"]
+
+# GPIO PINS
+MOTOR = 00
---
+++ WiiMoteTest.py
@@ -0,0 +1,11 @@
+# Code for Rasberry Pi to interface with a VESC using a Wiimote
+
+import time
+import sys
+import threading
+import subprocess
+import cwiid
+
+# Global Variables
+WIIMOTE_MAC = "00:1C:BE:25:8B:36"
+POWER_DOWN = ["sudo", "shutdown", "-h", "now"]

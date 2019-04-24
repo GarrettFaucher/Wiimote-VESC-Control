950154c1ff358a48942259d1b15a244730f5600e
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-04-24 13:52:22 -0400

    Update VESCReceive.py

--- VESCReceive.py
+++ VESCReceive.py
@@ -1,10 +1,7 @@
 # Code for Rasberry Pi to interface with a VESC using a Wiimote
 
 import time
-#import sys
-#import threading
-#import subprocess
-import pigpio
+import serial
 import pyvesc
 import sys
 from subprocess import Popen, PIPE
@@ -16,11 +13,24 @@ WIIMOTE_MAC = "00:1C:BE:25:8B:36"
 POWER_DOWN = ["sudo", "shutdown", "-h", "now"]
 
 # EXPERIMENTAL CODE ALL BELOW
-BRAKE = pyvesc.SetCurrentBrake(1)
+BRAKE_ONE = pyvesc.SetCurrentBrake(10)
+BRAKE_TWO = pyvesc.SetCurrentBrake(100)
+BRAKE_THREE = pyvesc.SetCurrentBrake(10000)
+BRAKE_FOUR = pyvesc.SetCurrentBrake(100000)
 SPEED_ONE = pyvesc.SetDutyCycle(100)
 SPEED_TWO = pyvesc.SetDutyCycle(1000)
 SPEED_THREE = pyvesc.SetDutyCycle(10000)
 SPEED_FOUR = pyvesc.SetDutyCycle(100000)
+
+# Create a serial object to send serial messages
+ser = serial.Serial(
+    port='/dev/serial0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
 def printOutput(message):
     print()
@@ -46,12 +56,32 @@ while True:
         sys.stdout.write(str(newInput))
         sys.stdout.flush()
 
-
-// Motor Control
+go = True
+# Motor Control
 while True:
-    // Brake
-    if OUTPUT.A == pressed:
-        pyvesc.SetCurrentBrake(???)
-    if OUTPUT.B == pressed and OUTPUT.DOWN == pressed:
-        pyvesc.
-    
+    # Is it Brake or Accel?
+    if OUTPUT.A == pressed and go:
+        go = False
+    if OUTPUT.A == pressed and not go:
+        go = True
+
+    #Brake Mode
+    if not go:
+        if OUTPUT.B == pressed and OUTPUT.BTN_DOWN == pressed:
+            ser.write(pyvesc.encode(BRAKE_ONE))
+        if OUTPUT.B == pressed and OUTPUT.BTN_LEFT == pressed:
+            ser.write(pyvesc.encode(BRAKE_TWO))
+        if OUTPUT.B == pressed and OUTPUT.BTN_UP == pressed:
+            ser.write(pyvesc.encode(BRAKE_THREE))
+        if OUTPUT.B == pressed and OUTPUT.BTN_RIGHT == pressed:
+            ser.write(pyvesc.encode(BRAKE_FOUR))
+    #Accel Mode
+    if go:
+        if OUTPUT.B == pressed and OUTPUT.BTN_DOWN == pressed:
+            ser.write(pyvesc.encode(SPEED_ONE))
+        if OUTPUT.B == pressed and OUTPUT.BTN_LEFT == pressed:
+            ser.write(pyvesc.encode(SPEED_TWO))
+        if OUTPUT.B == pressed and OUTPUT.BTN_UP == pressed:
+            ser.write(pyvesc.encode(SPEED_THREE))
+        if OUTPUT.B == pressed and OUTPUT.BTN_RIGHT == pressed:
+            ser.write(pyvesc.encode(SPEED_FOUR))

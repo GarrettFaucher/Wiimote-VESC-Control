f162693ee523fa2fb5ea59b3f995a94e24001f7d
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-05-01 13:27:29 -0400

    Slow accel

--- VESCReceiveWORKING.py
+++ VESCReceiveWORKING.py
@@ -13,18 +13,16 @@ WIIMOTE_MAC = "00:1C:BE:25:8B:36"
 POWER_DOWN = ["sudo", "shutdown", "-h", "now"]
 
 # EXPERIMENTAL CODE ALL BELOW
-BRAKE_ONE = 10000
-BRAKE_TWO = 20000
-BRAKE_THREE = 30000
-BRAKE_FOUR = 50000
-SPEED_ONE = 10000
-SPEED_TWO = 20000
-SPEED_THREE = 30000
-SPEED_FOUR = 50000
+BRAKE_ONE = pyvesc.SetCurrentBrake(10000)
+BRAKE_TWO = pyvesc.SetCurrentBrake(20000)
+BRAKE_THREE = pyvesc.SetCurrentBrake(30000)
+BRAKE_FOUR = pyvesc.SetCurrentBrake(50000)
+SPEED_ONE = pyvesc.SetDutyCycle(10000)
+SPEED_TWO = pyvesc.SetDutyCycle(20000)
+SPEED_THREE = pyvesc.SetDutyCycle(30000)
+SPEED_FOUR = pyvesc.SetDutyCycle(50000)
 
-STOP = 0
-
-SLEEP_TIME = 0.0001
+STOP = pyvesc.SetDutyCycle(0)
 
 # Create a serial object to send serial messages
 ser = serial.Serial(
@@ -42,19 +40,11 @@ def enqueueOutput(out, queue):
         out.flush()
         queue.put(lines)
 
-def changeDuty(num):
-    ser.write(pyvesc.encode(pyvesc.SetDutyCycle(num)))
-
-def changeBrake(num):
-    ser.write(pyvesc.encode(pyvesc.SetCurrentBrake(num)))
-
 process = Popen(['/usr/bin/node', 'pipe.js'], stdout=PIPE)
 queue = Queue()
 enqueueOutputThread = Thread(target=enqueueOutput, args=(process.stdout, queue))
 enqueueOutputThread.daemon = True
 enqueueOutputThread.start()
-
-oldInput = ""
 
 go = True
 tick = 0
@@ -79,41 +69,31 @@ while True:
     #Brake Mode
     if not go:
         if newInput == "DOWN":
-            changeBrake(BRAKE_ONE)
+            ser.write(pyvesc.encode(BRAKE_ONE))
             print(" - Brake 1")
         if newInput == "LEFT":
-            changeBrake(BRAKE_TWO)
+            ser.write(pyvesc.encode(BRAKE_TWO))
             print(" - Brake 2")
         if newInput == "UP":
-            changeBrake(BRAKE_THREE)
+            ser.write(pyvesc.encode(BRAKE_THREE))
             print(" - Brake 3")
         if newInput == "RIGHT":
-            changeBrake(BRAKE_FOUR)
+            ser.write(pyvesc.encode(BRAKE_FOUR))
             print(" - Brake 4")
 
     #Accel Mode
     if go:
         if newInput == "DOWN":
-            changeDuty(SPEED_ONE)
+            ser.write(pyvesc.encode(SPEED_ONE))
             print(" - Accel 1")
-
         if newInput == "LEFT":
-            if oldInput = "DOWN":
-                for i in range(SPEED_ONE, SPEED_TWO):
-                    changeDuty(i)
-                    time.sleep(SLEEP_TIME)
-            changeDuty(SPEED_TWO)
+            ser.write(pyvesc.encode(SPEED_TWO))
             print(" - Accel 2")
-
         if newInput == "UP":
-            for i in range(SPEED_TWO, SPEED_THREE):
-                changeDuty(i)
-                time.sleep(SLEEP_TIME) # CHANGE ME LATER
-            changeDuty(SPEED_THREE)
+            ser.write(pyvesc.encode(SPEED_THREE))
             print(" - Accel 3")
-
         if newInput == "RIGHT":
-            changeDuty(SPEED_FOUR)
+            ser.write(pyvesc.encode(SPEED_FOUR))
             print(" - Accel 4")
 
     if newInput == "HOME":
@@ -121,5 +101,3 @@ while True:
 
     if tick > 0:
         tick -= 1
-
-    oldInput = newInput

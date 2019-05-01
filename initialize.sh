a8dcc016183f8cb3f3e4bb9b87c7ce0f69c78fa0
Garrett Faucher <44652299+GarrettFaucher@users.noreply.github.com>
2019-05-01 12:49:30 -0400

    Delete initialize.sh

--- initialize.sh
+++
@@ -1,5 +0,0 @@
-# Starts WiiMote listener, pipe, and VESC Reciever in a monitored environment via pm2
-# If the software crashes, pm2 will restart the script immediately
-# Run this file from /etc/rc.local on raspberrypi bootup
-
-sudo pm2 start VESCReceive.py -n "Wiimote-VESC-Control"

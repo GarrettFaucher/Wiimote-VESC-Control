8f780a32e2a77d5ea89e3800a72b6ce7f0595e2b
Garrett Faucher <44652299+GarrettFaucher@users.noreply.github.com>
2019-05-02 11:56:30 -0400

    Update VESCReceive.py

--- VESCReceive.py
+++ VESCReceive.py
@@ -22,9 +22,8 @@ SPEED_THREE = 30000
 SPEED_FOUR = 50000
 STOP = 0
 
-DIV_NUM = 10
-
-SLEEP_TIME = 0.01
+DIV_CONST = 15
+SLEEP_TIME = 0.0001
 
 # Create a serial object to send serial messages
 ser = serial.Serial(
@@ -36,19 +35,40 @@ ser = serial.Serial(
     timeout=1
 )
 
+# Function to read input
 def enqueueOutput(out, queue):
     while True:
         lines = out.readline().decode('utf-8')
         out.flush()
         queue.put(lines)
 
+# Function to simplify the serial output for changing duty cycles
 def changeDuty(num):
     ser.write(pyvesc.encode(pyvesc.SetDutyCycle(num)))
     time.sleep(SLEEP_TIME)
 
+# Function to simplify the serial output for changing brake
 def changeBrake(num):
     ser.write(pyvesc.encode(pyvesc.SetCurrentBrake(num)))
 
+# Function to simplify the gradual increase of speed if stepping up
+def stepUp(speedStart, speedEnd):
+    for i in range(int(speedStart/DIV_CONST), int(speedEnd/DIV_CONST)):
+        changeDuty(i*DIV_CONST)
+        try:
+            newInput = str(queue.get_nowait()).rstrip()
+        except Empty:
+            continue
+        else:
+            sys.stdout.write(newInput + "\n")
+            sys.stdout.flush()
+
+        if newInput == "A":
+            break
+
+
+
+# Starting the input process and thread
 process = Popen(['/usr/bin/node', 'pipe.js'], stdout=PIPE)
 queue = Queue()
 enqueueOutputThread = Thread(target=enqueueOutput, args=(process.stdout, queue))
@@ -56,11 +76,12 @@ enqueueOutputThread.daemon = True
 enqueueOutputThread.start()
 
 oldInput = ""
-
 go = True
 tick = 0
+
 # Motor Control
 while True:
+    # Get new input
     try:
         newInput = str(queue.get_nowait()).rstrip()
     except Empty:
@@ -79,6 +100,7 @@ while True:
 
     #Brake Mode
     if not go:
+        changeBrake(10)
         if newInput == "DOWN":
             changeBrake(BRAKE_ONE)
         if newInput == "LEFT":
@@ -90,44 +112,46 @@ while True:
 
     #Accel Mode
     if go:
+        # Speed 1
         if newInput == "DOWN":
             changeDuty(SPEED_ONE)
 
+        # Speed 2
         if newInput == "LEFT":
             if oldInput == "DOWN":
-                for i in range(SPEED_ONE, SPEED_TWO):
-                    changeDuty(i)
-            changeDuty(SPEED_TWO)
+                stepUp(SPEED_ONE, SPEED_TWO)
+            else:
+                changeDuty(SPEED_TWO)
 
+        # Speed 3
         if newInput == "UP":
             if oldInput == "LEFT":
-                for i in range(SPEED_TWO, SPEED_THREE):
-                    changeDuty(i)
-            if oldInput == "DOWN":
-                for i in range(SPEED_ONE, SPEED_THREE):
-                    changeDuty(i)
-            changeDuty(SPEED_THREE)
+                stepUp(SPEED_TWO, SPEED_THREE)
+            elif oldInput == "DOWN":
+                stepUp(SPEED_ONE, SPEED_THREE)
+            else:
+                changeDuty(SPEED_THREE)
 
+        # Speed 4
         if newInput == "RIGHT":
             if oldInput == "UP":
-                for i in range(SPEED_THREE, SPEED_FOUR):
-                    changeDuty(i)
-            if oldInput == "LEFT":
-                for i in range(SPEED_TWO, SPEED_FOUR):
-                    changeDuty(i)
-            if oldInput == "DOWN":
-                for i in range(SPEED_ONE, SPEED_FOUR):
-                    changeDuty(i)
-            changeDuty(SPEED_FOUR)
+                stepUp(SPEED_THREE, SPEED_FOUR)
+            elif oldInput == "LEFT":
+                stepUp(SPEED_TWO, SPEED_FOUR)
+            elif oldInput == "DOWN":
+                stepUp(SPEED_ONE, SPEED_FOUR)
+            else:
+                changeDuty(SPEED_FOUR)
 
+    # Emergency off switch
     if newInput == "HOME":
             changeDuty(STOP)
 
-    if tick > 0:
-        tick -= 1
-
+    # If controller is disconnected stop the script
     if newInput == "DISCONNECTED":
         exit()
 
-
     oldInput = newInput
+
+    if tick > 0:
+        tick -= 1
